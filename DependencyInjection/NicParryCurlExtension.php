fd297406f6826df3e66ce7664fd501a7113608ea
Nic Parry <nicholasp@synaq.com>
2012-11-02 15:08:58 +0200

    Added DI

---
+++ DependencyInjection/NicParryCurlExtension.php
@@ -0,0 +1,12 @@
+<?php
+namespace NicParry\Bundle\NicParryCurlBundle\DependencyInjection;
+
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class AcmeHelloExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+    }
+}
